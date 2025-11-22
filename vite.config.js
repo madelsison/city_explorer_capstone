4e84cf14c4f9f3dd36f417bda56a1fd98ec500fb
Madel Lachica Sison <88952848+madelsison@users.noreply.github.com>
2025-11-22 22:22:06 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/madel_cityExplorer/",
+  base: "/city_explorer_capstone/",
 });
